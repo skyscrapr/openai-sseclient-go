84d88217d8eed1df675262cbd6cc42373bd66515
Richard Weerasinghe <rich.w@me.com>
2023-06-19 12:54:27 +1200

    fix example scaffold

--- example/main.go
+++ example/main.go
@@ -8,32 +8,32 @@ package main
 // 	"github.com/skyscrapr/openai-sdk-go/openai"
 // )
 
-// func main() {
-// 	ftreq := openai.CreateFineTunesRequest{
-// 		TrainingFile: "file-QotXmqJbY8PPgDmjOcRMe6E4",
-// 		ValidationFile: "file-0J5DkJ7KWC1QOyBJtb5r66dj",
-// 	}
-// 	fineTune, err := client.FineTunes().CreateFineTune(&ftreq)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Printf("finetune-id: %s - state: %s \n", fineTune.Id, fineTune.Status)
-// 	err = client.FineTunes().SubscribeFineTuneEvents(fineTune.Id, fineTuneEventHandler, eventErrorHandler)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
+func main() {
+	// 	ftreq := openai.CreateFineTunesRequest{
+	// 		TrainingFile: "file-QotXmqJbY8PPgDmjOcRMe6E4",
+	// 		ValidationFile: "file-0J5DkJ7KWC1QOyBJtb5r66dj",
+	// 	}
+	// 	fineTune, err := client.FineTunes().CreateFineTune(&ftreq)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("finetune-id: %s - state: %s \n", fineTune.Id, fineTune.Status)
+	// 	err = client.FineTunes().SubscribeFineTuneEvents(fineTune.Id, fineTuneEventHandler, eventErrorHandler)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
 
-//     fmt.Println("Done!")
-// }
+	//     fmt.Println("Done!")
+	// }
 
-// func eventErrorHandler(err error) error {
-// 	log.Printf("error : %s", err)
-// 	return err
-// }
+	// func eventErrorHandler(err error) error {
+	// 	log.Printf("error : %s", err)
+	// 	return err
+	// }
 
-// func fineTuneEventHandler(event *openai.SSEEvent) error {
-// 	var fineTuneEvent openai.FineTuneEvent
-// 	err := json.Unmarshal([]byte(event.Data), &fineTuneEvent)
-// 	log.Printf("Event : %s", fineTuneEvent.Message)
-// 	return err
-// }
+	//	func fineTuneEventHandler(event *openai.SSEEvent) error {
+	//		var fineTuneEvent openai.FineTuneEvent
+	//		err := json.Unmarshal([]byte(event.Data), &fineTuneEvent)
+	//		log.Printf("Event : %s", fineTuneEvent.Message)
+	//		return err
+}
